39d765425326da4c11aebea9335e76e9df114cd4
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-10-02 04:13:53 +0100

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,7 +5,7 @@ const jwt=require("jsonwebtoken")
 const nodemailer=require("nodemailer")
 
 const createToken=(_id)=>{
-    return jwt.sign({_id},process.env.SECRET,{expiresIn:'1d'})
+return jwt.sign({_id},process.env.SECRET,{expiresIn:'1d'})
 }
 //Signing up users
 const signupUser=async(req,res)=>{
